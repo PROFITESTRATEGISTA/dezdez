24c16f4a6815cb6d87b5285d1fd644bf65613394
PROFITESTRATEGISTA <PROFITESTRATEGISTA@GMAIL.COM>
2025-07-18 19:23:31 -0300

    Fix import paths in src files to reference root directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import AppLayout from './components/layout/AppLayout';
-import HomePage from './components/HomePage';
-import ClientArea from './components/client/ClientArea';
-import AdminPanel from './components/AdminPanel';
-import PaymentPage from './components/PaymentPage';
-import QuestionarioMedico from './pages/QuestionarioMedico';
-
-function App() {
-  return (
-    <AuthProvider>
-      <Router>
-        <AppLayout>
+import { AuthProvider, useAuth } from '../context/AuthContext';
+import { clientsDatabase } from '../data/clientsDatabase';
+import LocalSEO from '../components/LocalSEO';
+import AppLayout from '../components/layout/AppLayout';
+import HomePage from '../components/HomePage';
+import AdminPanel from '../components/AdminPanel';
+import ClientArea from '../components/client/ClientArea';
+import ClientLogin from '../components/ClientLogin';
+import PaymentPage from '../components/PaymentPage';
+import BenefitsPage from '../components/BenefitsPage';
+import MedicalQuestionnaire from '../components/MedicalQuestionnaire';
+import LeadCollection from '../components/LeadCollection';
+import CheckoutCalculator from '../components/CheckoutCalculator';
+import LocationPage from '../components/LocationPages';
+import FloatingWhatsApp from '../components/FloatingWhatsApp';
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/area-cliente/*" element={<ClientArea />} />
@@ -24,6 +24,6 @@ function App() {
       </Router>
     </AuthProvider>
   );
+import { BillingPeriod, CheckoutData } from '../types';
 }
-
 export default App;
